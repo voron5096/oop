959f714189920b624e6a24d23b9d2ed463ee0799
voron5096 <70198995+voron5096@users.noreply.github.com>
2023-04-15 13:46:30 +0000

    3.1.4 original

--- 314old.cpp
+++ 314old.cpp
@@ -3,10 +3,10 @@ using namespace std;
 #include "314old.h"
 
 void func(MyClass obj) {
-  obj.method1();
+  obj.method2();
   obj.print();
   obj.sum();
-  obj.method2();
+  obj.method1();
   obj.print(); // вывод содержимого массива второго объекта
   obj.sum(); // вывод суммы элементов массива второго объекта
 }
@@ -21,11 +21,11 @@ int main() {
   }
   
   cout << size << endl; // вывод значения размерности массива
-  MyClass *obj1 = new MyClass; // создание первого объекта
+  MyClass *obj_ = new MyClass; // создание первого объекта
   MyClass obj(size); // присвоение первому объекту результата работы функции func с аргументом, содержащим значение размерности массива
-  obj.input(); // для первого объекта вызов метода создания массива.
-
+  delete (obj_);
   cout << endl;
+  obj.input(); // для первого объекта вызов метода создания массива??
   func(obj);
   cout << endl;
   return(0);
